3fbab5fd3b1e1fcbb602a1fee648fd9c86ba7a95
黄卓琳 <61574030+Jonily@users.noreply.github.com>
2020-12-15 20:58:38 +0800

    Add files via upload

---
+++ SQL.sql
@@ -0,0 +1,15 @@
+create table Stu(
+name varchar(20),
+school varchar(50),
+obiect varchar(20),
+sex varchar(10),
+
+	);
+create table exam(
+id int,
+name varchar,
+math decimal,
+english decimal,
+);
+
+select * from exam order by chinese desc limit 1,1
